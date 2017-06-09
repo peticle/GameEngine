f7c4f36ee03de259dc8bbefa6e61585c0feeec3c
Clément Petignat <clement.petignat@gmail.com>
2017-06-09 14:16:30 +0200

    Implémentation des getters et setters pour img

--- GameEngine/Img.cpp
+++ GameEngine/Img.cpp
@@ -21,14 +21,22 @@ Img::~Img() {
 }
 
 /*** Getters ***/
-SDL_Texture *Img::getTexture() const {}
+SDL_Texture *Img::getTexture() const {
+	return textu;
+}
 
-SDL_Rect Img::getRect() const {}
+SDL_Rect Img::getRect() const {
+	return rect;
+}
 
 /*** Setters ***/
-void Img::setTexture(SDL_Texture *textu) {}
+void Img::setTexture(SDL_Texture *textu) {
+	this->textu = textu;
+}
 
-void Img::setRect(SDL_Rect rect) {}
+void Img::setRect(SDL_Rect rect) {
+	this->rect = rect;
+}
 
 /*** Public functions ***/
 void Img::draw(Vector2 pos, SDL_Renderer *render) {}
