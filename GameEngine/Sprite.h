3f1a36f4de5486016b81a6bc127571bdeb351b46
Clément Petignat <clement.petignat@gmail.com>
2017-06-09 16:20:33 +0200

    Ajout de la base du fichier header pour sprite

--- GameEngine/Sprite.h
+++ GameEngine/Sprite.h
@@ -2,14 +2,59 @@
 #ifndef SPRITE_H
 #define SPRITE_H
 
+#include <vector>
+#include "Img.h"
+
 // Manage the sprites
 class Sprite {
 public:
 
 	/*** Constructors ***/
+
 	Sprite();
+	Sprite(Img *image, Vector2 firstFramePos, Vector2 frameSize, unsigned int frameNumber, SDL_Renderer *render);
+	Sprite(Img *image, Vector2 firstFramePos, Vector2 frameSize, unsigned int frameNumber, bool loop, bool pause, SDL_Renderer *render);
+	Sprite(Img *image, Vector2 firstFramePos, Vector2 frameSize, unsigned int frameNumber, bool loop, bool pause, bool rotate, SDL_Renderer *render);
+	Sprite(Img *image, Vector2 firstFramePos, Vector2 frameSize, unsigned int frameNumber, bool rotate, SDL_Renderer *render);
+
+	/*** Destructors ***/
+
+	~Sprite();
+
+	/*** Getters ***/
+
+	Vector2 getFirstFramePos() const;
+	Vector2 getFrameSize() const;
+	unsigned int getFrameNumber() const;
+	bool getRotate() const;
+	bool getLoop() const;
+	bool getPause() const;
+
+	/*** Setters ***/
+
+	void setFirstFramePos(Vector2 pos);
+	void setFrameSize(Vector2 size);
+	void setFrameNumber(unsigned int frameNumber);
+	void setRotate(bool rotate);
+	void setLoop(bool loop);
+	void setPause(bool pause);
+
+	/*** Public functions ***/
+
+	// Update the frame of the sprite
+	void update();
 
 private:
+
+	/*** Variables ***/
+
+	Vector2 firstFramePos;
+	Vector2 frameSize;
+	unsigned int frameNumber;
+	bool rotate;
+	bool loop;
+	bool pause;
+	unsigned long timePassed;
 };
 
 #endif // SPRITE_H
