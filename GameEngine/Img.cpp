77e336ce361b29cb771da894fa5a2ed59a7b0d7e
Clément Petignat <clement.petignat@gmail.com>
2017-06-09 14:12:55 +0200

    Implémentation des getters et setters pour img

--- GameEngine/Img.cpp
+++ GameEngine/Img.cpp
@@ -1,12 +1,24 @@
 #include "Img.h"
 
+#include <SDL_image.h>
+
 /*** Constructors ***/
 Img::Img() {}
 
-Img::Img(std::string fileName, SDL_Renderer *render) {}
+Img::Img(std::string fileName, SDL_Rect rect, SDL_Renderer *render) {
+
+	fileName = "img/" + fileName;
+	SDL_Surface *loadSurf = IMG_Load(fileName.c_str());
+	textu = SDL_CreateTextureFromSurface(render, loadSurf);
+	SDL_FreeSurface(loadSurf);
+
+	setRect(rect);
+}
 
 /*** Destructors ***/
-Img::~Img() {}
+Img::~Img() {
+	SDL_DestroyTexture(textu);
+}
 
 /*** Getters ***/
 SDL_Texture *Img::getTexture() const {}
